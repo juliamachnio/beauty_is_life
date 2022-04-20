90236668f76e7e9f345b4c0ae2fc1369c468c0df
juliamachnio <julia.machnio1999@gmail.com>
2022-04-20 21:42:05 +0200

    our beauty app, first commit

---
+++ jm_mn_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class JmMnAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'jm_mn_app'
---
+++ jm_mn_app/models.py
@@ -0,0 +1,41 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+    #klient
+    #usługa
+    #zabiegi (harmonogram - o było i będzie)
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    mail = models.EmailField()
+    date = models.DateTimeField()
+
+
+    # def publish(self):
+    #     self.published_date = timezone.now()
+    #     self.save()
+
+    def __str__(self):
+        return self.name
+
+
+class Service(models.Model):
+    service_name = models.CharField(max_length=100)
+    service_description = models.TextField()
+    price = models.IntegerField()
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return self.service_name
+
+
+class RequestsRegister(models.Model):
+    customer = models.ForeignKey('Customer', on_delete=models.CASCADE)
+    service = models.ForeignKey('Service', on_delete=models.CASCADE)
+    service_date = models.DateTimeField(default=timezone.now)
+
+
+    def __str__(self):
+        return self.service_name
